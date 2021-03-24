c602713d3e3fcd0decbc3a723553d427e772707a
Babacar Ndoye <babacarndoye13@gmail.com>
2021-03-24 03:54:08 -0400

    No longer making board class parent

---
+++ Board.py
@@ -0,0 +1,11 @@
+# getMoveNUmber
+
+# getBoard
+
+# Player will get these
+
+class Board:
+    def __init__(self):
+        self.player1 = None
+        self.player2 = None
+        self.moveNumber = 0
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from Computer import Computer
 from Human import Human
 from Player import Player
 
-obj1 = Human("Bob")
-obj1.get_name()
+obj1 = Human("Test")
+print(obj1.make_move())
 
-obj2 = Computer("Fake Bob")
-obj2.get_name()
+obj2 = Computer("Computer")
+print(obj2.make_move())
